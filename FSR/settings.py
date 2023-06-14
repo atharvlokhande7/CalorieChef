f1368c8a42486d1caaf20ada684ef761925b135f
atharvlokhande7 <72869554+atharvlokhande7@users.noreply.github.com>
2023-06-14 18:42:57 +0530

    Update settings.py

--- FSR/settings.py
+++ FSR/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'FSR.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'FRS',
+        'USER': 'atharv',
+        'PASSWORD': 'lokhande',
+        'HOST': '<hostname>',
+        'PORT': '<port_number>',
     }
 }
+
 
 
 # Password validation
