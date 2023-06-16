f8bde9dc3cc7037bb4d34cdef34b630db23f1d6d
atharvlokhande7 <72869554+atharvlokhande7@users.noreply.github.com>
2023-06-16 11:21:38 +0530

    Delete main directory

--- main/querydemo.py
+++
@@ -1,9 +0,0 @@
-'''
-From main.model import *
-users= account.objects.all()
-userbyname= account.objects.get(username="Altair")
-
-r1= recipe.objects.get(id=1)
-r1.hcon.all()
-recipe = recipe.objects.filter(rtype="B")
-'''
